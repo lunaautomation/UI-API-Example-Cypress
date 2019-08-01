2888df453a720c9c8229a7ad03ba6846efa5f384
Dan Hitchcock <33898935+lunaautomation@users.noreply.github.com>
2019-08-01 16:43:45 +0100

    Added some more screenshot checking Updated the challenging dom click check

--- cypress/integration/testingFeatures.js
+++ cypress/integration/testingFeatures.js
@@ -3,30 +3,39 @@ describe('UI Tests', function () {
     cy.visit("https://the-internet.herokuapp.com/")
   });
 
-  it.skip('Check Challenging DOM page has\'t changed', () => {
+  it('Check Challenging DOM page has not changed using screenshot comparison', () => {
     cy.contains('Challenging DOM').click()
       .then(() => {
         cy.document()
           .toMatchImageSnapshot();
       });
+      cy.get('.button.alert').click()
+      .then(() => {
+        cy.document()
+          .toMatchImageSnapshot();
+      });
+
   });
 
-  it.skip('Check element id changes after red button click', function () {
+  it('Check element id changes after red button click', function () {
     cy.contains('Challenging DOM').click();
     //State Before
-    cy.get('.button').as('buttonBefore');
+    cy.get('.button').first().as('buttonBefore');
+    cy.get('.button.alert').as('alertButton');
+    cy.get('.button.success').as('successButton')
     cy.get('.button.alert').click();
     //State After 
-    cy.get('.button').as('buttonAfter')
+    cy.get('.button').first().as('buttonAfter')
+    cy.get('.button.alert').as('alertButtonAfter');
+    cy.get('.button.success').as('successButtonAfter')
       .get('@buttonBefore','@buttonAfter').then( () => {
         assert.notEqual(this.buttonBefore[0].id, this.buttonAfter[0].id);
-        assert.notEqual(this.buttonBefore[1].id, this.buttonAfter[1].id);
-        assert.notEqual(this.buttonBefore[2].id, this.buttonAfter[2].id);
+        assert.notEqual(this.alertButton[0].id, this.alertButtonAfter[0].id);
+        assert.notEqual(this.successButton[0].id, this.alertButtonAfter[0].id);
       })
   });
 
-  it('Check \'Hello World\' appears after AJAX loader', function () {
-
+  it('Check \'Hello World\' appears after loader', function () {
     cy.contains('Dynamic Loading').click();
     cy.contains('Example 2: Element rendered after the fact').click();
     cy.get('#start > button').click()
